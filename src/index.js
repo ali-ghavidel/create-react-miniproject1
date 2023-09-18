de5c214c6397eda5508b65cc71bb0e1521b7741b
ali-ghavidel <ghavidel.aliabadi@gmail.com>
2023-09-18 20:56:05 +0330

    cs #7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const tick = () => {
+  const elem = (
+    <div>
+      <h1>سلام دوستان عزیزم</h1>
+      <h2>it is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(()=>{
+  tick()
+}, 1000);
+
+
+
+
